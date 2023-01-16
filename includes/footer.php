1b7f7726638cce15d71619c2c689df16d8411d30
Brian Angulo <bangulo219@gmail.com>
2023-01-16 13:54:20 -0600

    update: cleaning up footer and header

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,6 @@
 <div class="container">
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-        </ul>
-        <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+    <footer class="py-3 my-4 border-top">
+        <p class="text-center text-muted">&copy; 2023 by Brian</p>
     </footer>
 </div>
 </body>
--- includes/header.php
+++ includes/header.php
@@ -7,17 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Random Quotes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
 <div class="container">
-    <header class="d-flex justify-content-center py-3">
-        <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-        </ul>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 mb-4 border-bottom">
+      <ul class="nav col-12 col-md-auto justify-content-end mb-md-0">
+        <li><a href="https://github.com/brianangulo/quotes_site" class="nav-link px-2 link-dark">Github</a></li>
+      </ul>
     </header>
-</div>
+  </div>
