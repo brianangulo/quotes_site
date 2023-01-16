3386f8351a40b6fafd3b0c6c87b0b8295e9af616
Brian Angulo <bangulo219@gmail.com>
2023-01-16 13:54:45 -0600

    update: cleaning up quotes main div

--- public/index.php
+++ public/index.php
@@ -1,28 +1,27 @@
 <?php
-    require __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/requires.php';
-    use App\Utils;
-    use App\Api;
+require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/requires.php';
 
-    $quotableApi = 'https://quotable.io/random';
-    $response = Api::getJson($quotableApi);
-    $quote;
-    if ($response !== null) {
-        $quote = '"'.$response['content'].'"'.' -'.$response['author'];
-    } else {
-        $quote = 'Error occurred sorry, please try again.';
-    }
+use App\Utils;
+use App\Api;
+
+$quotableApi = 'https://quotable.io/random';
+$response = Api::getJson($quotableApi);
+$quote;
+if ($response !== null) {
+    $quote = '"' . $response['content'] . '"' . ' -' . $response['author'];
+} else {
+    $quote = 'Error occurred sorry, please try again.';
+}
 ?>
 
 <!--header-->
 <?php Utils::includes('header.php'); ?>
+<div class="container container-fluid min-vh-100 justify-content-center d-flex flex-column text-center">
+    <h2 class="text-primary pb-4">Refresh for quotes</h2>
 
-<h1 class="name">Fresh random quotes</h1>
-
-<!-- quote -->
-<h4><?php echo $quote ?></h4>
-
+    <!-- quote -->
+    <h4><?php echo $quote ?></h4>
+</div>
 <!--footer-->
 <?php Utils::includes('footer.php'); ?>
-
-<script src="js/script.js"></script>
