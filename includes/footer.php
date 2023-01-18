ac0c9ef33e5a22893320c72a4eb76a4f958b608a
Brian Angulo <bangulo219@gmail.com>
2023-01-17 22:05:49 -0600

    update: removing unused scripts

--- includes/footer.php
+++ includes/footer.php
@@ -5,5 +5,4 @@
 </div>
 </body>
 </html>
-<script src="https://code.jquery.com/jquery-3.6.3.slim.min.js" integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
