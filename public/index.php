3430590006029c3d2364b044ab1569c728704e6d
Brian Angulo <bangulo219@gmail.com>
2023-03-20 23:30:10 -0500

    update: adding and configuring router on the index page

--- public/index.php
+++ public/index.php
@@ -1,24 +1,19 @@
 <?php
+// Require composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/requires.php';
+// required app loaders and globals
+require __DIR__ . '/../src/requires.php';
 
-use App\Utils;
-use App\Quotes;
+use Bramus\Router\Router;
+use \App\api\Api;
 
-$quote = Quotes::getRandomQuotable();
-?>
+// Create Router instance
+$router = new Router();
 
-<!--header-->
-<?php Utils::includes('header.php'); ?>
-<div class="container container-fluid min-vh-100 justify-content-center align-item-center d-flex flex-column text-center">
-    <h2 class="text-primary pb-4">Refresh for quotes</h2>
-    <!-- quote -->
-    <h4 id="quote"><?php echo $quote ?></h4>
-    <button id="refreshButton" onclick="refreshQuote()" style="max-width: fit-content" type="button" class="btn btn-primary btn-md align-self-center mt-4">
-        <span hidden class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-        New Quote
-    </button>
-</div>
-<!--footer-->
-<?php Utils::includes('footer.php'); ?>
-<script defer src="js/script.js"></script>
+$router->get('/', function () {
+    require __DIR__ . '/../src/views/index.php';
+});
+
+$router->get('/api/random-quote', [Api::class, 'randomQuote']);
+
+$router->run();
