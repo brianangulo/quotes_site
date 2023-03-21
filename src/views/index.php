41b6e4c93d6935e5326b02c5659504be28c77a41
Brian Angulo <bangulo219@gmail.com>
2023-03-20 23:27:22 -0500

    update: moved index page to views folder

---
+++ src/views/index.php
@@ -0,0 +1,21 @@
+<?php
+use App\Utils;
+use App\Quotes;
+
+$quote = Quotes::getRandomQuotable();
+?>
+
+<!--header-->
+<?php Utils::includes('header.php'); ?>
+<div class="container container-fluid min-vh-100 justify-content-center align-item-center d-flex flex-column text-center">
+    <h2 class="text-primary pb-4">Refresh for quotes</h2>
+    <!-- quote -->
+    <h4 id="quote"><?php echo $quote ?></h4>
+    <button id="refreshButton" onclick="refreshQuote()" style="max-width: fit-content" type="button" class="btn btn-primary btn-md align-self-center mt-4">
+        <span hidden class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        New Quote
+    </button>
+</div>
+<!--footer-->
+<?php Utils::includes('footer.php'); ?>
+<script defer src="js/script.js"></script>
