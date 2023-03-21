982acc3503f3990dfc627959604480cf76f7d66c
Brian Angulo <bangulo219@gmail.com>
2023-03-20 23:35:10 -0500

    update: setting up dotenv and moving define to constant

--- src/requires.php
+++ src/requires.php
@@ -1,4 +1,10 @@
 <?php
 // this file should be required in all other scripts to assure that common globals are accessible
-    // defining includes folder as a constant
-    define('INCLUDES_DIR', __DIR__.'/../includes/');
+// should be loaded after autoload
+
+use Dotenv\Dotenv;
+
+const INCLUDES_DIR = __DIR__ . '/../includes/';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
